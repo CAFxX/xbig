bdd52030303d2f5d43a512a0b7ec25fc0769bee3
Carlo Alberto Ferraris <cafxx@strayorange.com>
2025-04-30 15:59:15 +0900

    package doc

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package xbig provides a more ergonomic interface to the math/big package.
+// It provides functions for creating and manipulating big integers, floats, and rationals,
+// and for performing arithmetic operations on these types.
+package xbig
